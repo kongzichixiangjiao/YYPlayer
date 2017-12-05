be583e838fa417fe0577bd02c0f2349c564c9331
kongzichixiangjiao <58895347@qq.com>
2017-12-05 17:23:45 +0800

    播放器
     *  1、返回前一页
     *  2、上下渐变层
     *  3、遮罩imageView层
     *  4、播放暂停按钮
     *  5、屏幕中间播放按钮
     *  6、进度条
     *  7、播放时间显示
     *  8、大屏/小屏切换
     *  9、屏幕亮度调节
     *  10、系统声音调节
     *  11、快进/快退
     *  12、点击屏幕隐藏/展示上下渐变层

--- YYPlayer/ViewController.swift
+++ YYPlayer/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playBackView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initPlayerView()
     }
-
+    
+    func initPlayerView() {
+        self.playBackView.addSubview(pView)
+        
+        playBackView.addConstraint(NSLayoutConstraint(item: pView, attribute: .left, relatedBy: .equal, toItem: playBackView, attribute: .left, multiplier: 1, constant: 0))
+        playBackView.addConstraint(NSLayoutConstraint(item: pView, attribute: .top, relatedBy: .equal, toItem: playBackView, attribute: .top, multiplier: 1, constant: 0))
+        playBackView.addConstraint(NSLayoutConstraint(item: pView, attribute: .bottom, relatedBy: .equal, toItem: playBackView, attribute: .bottom, multiplier: 1, constant: 0))
+        playBackView.addConstraint(NSLayoutConstraint(item: pView, attribute: .right, relatedBy: .equal, toItem: playBackView, attribute: .right, multiplier: 1, constant: 0))
+    }
+    
+    lazy var pView: YYPlayerView = {
+        let v = YYPlayerView.loadPlayerView()
+        v.urlString = "http://qiniu.puxinasset.com/099f507aed7c5799d5ff40386a1a9615.mp4"
+        v.maskImageView.image = UIImage()
+        return v
+    }()
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
